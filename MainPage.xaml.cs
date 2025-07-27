db7577e5bd8ce44d0a20aa23caee6b20b3d787b9
Shafiq Jetha <1066864+fasterinnerlooper@users.noreply.github.com>
2025-07-27 10:15:07 -0600

    Fix comment URL lines

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -14,7 +14,6 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
-
 namespace DayByDay
 {
     /// <summary>
